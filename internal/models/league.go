119783b9fc5dda11fee066a249a286656a4fab8c
Berman Schultz <bermanschultz29@gmail.com>
2025-01-14 14:05:42 -0300

    feat: add league based application

--- internal/models/league.go
+++ internal/models/league.go
@@ -1,8 +1,13 @@
 package model
 
+import "github.com/google/uuid"
+
 type League struct {
 	Base
-	Name        string   `json:"name"`
-	SeasonCount int      `json:"season_count"`
-	Seasons     []Season `json:"season" gorm:"many2many:user_languages;"`
+	OwnerId uuid.UUID `json:"owner_id" gorm:"type:uuid; index"`
+	Owner   Player    `json:"owner" gorm:"foreignKey:OwnerId;references:ID"`
+	Name    string    `json:"name" gorm:"not null"`
+	Slug    string    `json:"slug" gorm:"not null; unique"`
+	Seasons []Season  `json:"season" gorm:"many2many:league_seasons;"`
+	Players []Player  `json:"players" gorm:"many2many:league_players;"`
 }
--- internal/models/player.go
+++ internal/models/player.go
@@ -2,15 +2,17 @@ package model
 
 type Player struct {
 	Base
-	Email       string `json:"email"`
-	Password    string `json:"-"`
-	Name        string `json:"name"`
-	Goals       int    `json:"goals"`
-	Assists     int    `json:"assists"`
-	Disarms     int    `json:"disarms"`
-	Dribbles    int    `json:"dribbles"`
-	Matches     int    `json:"matches"`
-	RedCards    int    `json:"red_cards"`
-	YellowCards int    `json:"yellow_cards"`
-	Position    string `json:"position"`
+	Email       string   `json:"email" binding:"required,email" gorm:"unique;not null"`
+	Password    string   `json:"-" binding:"required" gorm:"not null"`
+	Age         int      `json:"-" binding:"required" gorm:"check: age > 14"`
+	Name        string   `json:"name" binding:"required" gorm:"not null"`
+	Goals       int      `json:"goals" gorm:"deafult:0"`
+	Assists     int      `json:"assists" gorm:"deafult:0"`
+	Disarms     int      `json:"disarms" gorm:"deafult:0"`
+	Dribbles    int      `json:"dribbles" gorm:"deafult:0"`
+	Matches     int      `json:"matches" gorm:"deafult:0"`
+	RedCards    int      `json:"red_cards" gorm:"deafult:0"`
+	YellowCards int      `json:"yellow_cards" gorm:"deafult:0"`
+	Position    string   `json:"position"`
+	Leagues     []League `json:"leagues" gorm:"many2many:player_leagues"`
 }
