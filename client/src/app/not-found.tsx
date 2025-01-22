4f4c824c5c619ccac485c318c6a7d17c0a4f63ed
Berman Schultz <bermanschultz29@gmail.com>
2025-01-21 23:45:42 -0300

    feat: add not found page

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    //center the text
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <h1 className="text-4xl font-bold">Not Found</h1>
+      <p className="mt-4 text-lg text-gray-500">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
